2171bac9ed312a8c9a7e894f622768f33f24981d
Mille Elfver <mille.elfver98@gmail.com>
2024-08-09 10:14:49 +0200

    Add WeatherVM for data binding in WeatherWindow
    Introduce WeatherVM in WeatherApp.ViewModel namespace, implementing INotifyPropertyChanged for data binding. Update WeatherWindow.xaml to bind ListView's TextBlock to WeatherVM's Query property with UpdateSourceTrigger set to PropertyChanged. Add OnPropertyChanged method in WeatherVM to handle PropertyChanged event.

---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string _query;
+
+        public string Query
+        {
+            get { return _query; }
+            set
+            {
+                _query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
