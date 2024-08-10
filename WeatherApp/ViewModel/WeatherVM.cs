9a5f978d03d90ebed5a02a6c0956ab6255485dc2
Mille Elfver <mille.elfver98@gmail.com>
2024-08-10 11:02:00 +0200

    Add ObservableCollection<City> property to WeatherVM
    - Added `System.Collections.ObjectModel` to using directives.
    - Introduced `Cities` property of type `ObservableCollection<City>`.
    - Updated constructor to initialize `Cities` property.
    - Modified `MakeQuery` to clear and populate `Cities` with results from `AccuWeatherHelper.GetCities(Query)`.

--- WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace WeatherApp.ViewModel
                 OnPropertyChanged(nameof(Query));
             }
         }
+
+        public ObservableCollection<City> Cities { get; set; }
 
         private CurrentConditions _currentConditions;
 
@@ -72,11 +75,18 @@ namespace WeatherApp.ViewModel
             }
 
             SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
         }
 
         public async void MakeQuery()
         {
             var cities = await AccuWeatherHelper.GetCities(Query);
+
+            Cities.Clear();
+            foreach (var city in cities)
+            {
+                Cities.Add(city);
+            }
         }
 
 
